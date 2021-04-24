3f571e21d47a6b89a8837942aa2c279026f48da0
brian <brianhwang29@gmail.com>
2021-04-24 09:40:27 +0900

    front setup

---
+++ routes/page.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("profile", { title: "My Info - NodeBird", user: null });
+});
+
+router.get("/join", (req, res) => {
+  res.render("join", {
+    title: "Register - Nodebird",
+    user: null,
+    joinError: req.flash("joinError"),
+  });
+});
+
+router.get("/", (req, res, next) => {
+  res.render("main", {
+    title: "Nodebird",
+    twits: [],
+    user: null,
+    loginError: req.flash("loginError"),
+  });
+});
+
+module.exports = router;
