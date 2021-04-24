2e042014bc700490ecc60a05e4cce13418dd5042
brian <brianhwang29@gmail.com>
2021-04-24 11:27:51 +0900

    db setup

---
+++ models/post.js
@@ -0,0 +1,33 @@
+const Sequelize = require("sequelize");
+
+module.exports = class Post extends Sequelize.model {
+  static init(sequelize) {
+    return super.init(
+      {
+        content: {
+          type: Sequelize.STRING(140),
+          allowNUll: false,
+        },
+        img: {
+          type: Sequelize.STRING(200),
+          allowNUll: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "Post",
+        tableName: "posts",
+        paranoid: false,
+        charset: "utf-8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {
+    db.Post.belongsTo(db.User);
+    db.Post.belongsToMany(db.Hashtag, { through: "PostHashtag" });
+  }
+};
