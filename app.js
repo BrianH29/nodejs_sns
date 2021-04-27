54da986a824c3fcf93a511df33c5f61b20661b1e
BrianH <brianhwang29@gmail.com>
2021-04-27 17:13:32 +0900

    modified

--- app.js
+++ app.js
@@ -1,62 +1,58 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const morgan = require("morgan");
-const path = require("path");
-const session = require("express-session");
-const flash = require("flash");
-const passport = require("passport");
-require("dotenv").config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash'); 
+const {sequelize} = require('./models'); 
+const passport = require('passport'); 
+const dotenv = require('dotenv')
 
-const pageRouter = require("./routes/page");
-const { sequelize } = require("./models");
-const passportConfig = require("./passport");
+dotenv.config(); 
+const pageRouter = require('./routes/page'); 
+const passportConfig = require('./passport');
 
-const app = express();
+const app = express(); 
 sequelize.sync();
-passportConfig(passport);
+passportConfig();  
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-app.set("port", process.env.PORT || 3000);
+app.set('views',path.join(__dirname,'views')); 
+app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 3000);
 
-app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("nodebirdsecret"));
-app.use(
-  session({
-    resave: false,
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.json()); 
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser(process.env.COOKIE_SECRET)); 
+app.use(session({
+    resave:false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
     cookie: {
-      httpOnly: true,
-      secure: false,
+        httpOnly: true,
+        secure: false,
     },
-  })
-);
-
-app.use(flash());
-//req 요청에 passport 설정을 심는다.
+}))
+app.use(flash()); 
 app.use(passport.initialize());
-//req.session 객체에 passport 정보를 저장한다.
-app.use(passport.session());
+app.use(passport.session()); 
 
-app.use("/", pageRouter);
+app.use('/',pageRouter); 
 
 app.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
-
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-app.listen(app.get("port"), () => {
-  console.log("waiting on", app.get("port"));
-});
+    const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+  });
+  
+  app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+  app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기중');
+  });
