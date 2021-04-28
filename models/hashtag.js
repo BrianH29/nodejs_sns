e92a203100aca2e16b7c62ce505277a9e17d5360
BrianH <brianhwang29@gmail.com>
2021-04-28 10:28:54 +0900

    modified

--- models/hashtag.js
+++ models/hashtag.js
@@ -1,29 +1,25 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-module.exports = class Hashtag extends Sequelize.Model {
-  static init(sequelize) {
-    return super.init(
-      {
-        title: {
-          type: Sequelize.STRING(15),
-          allowNull: false,
-          uniqure: true,
-        },
-      },
-      {
-        sequelize,
-        timestamps: true,
-        underscored: false,
-        modelName: "Hashtag",
-        tableName: "hashtags",
-        paranoid: true,
-        charset: "utf8",
-        collate: "utf8_general_ci",
-      }
-    );
-  }
-
-  static associate(db) {
-    db.Hashtag.belongsToMany(db.Post, { through: "PostHashtag" });
-  }
-};
+module.exports = class Hashtag extends Sequelize.Model{
+    static init(sequelize){
+        return super.init({
+            title: {
+                type: Sequelize.STRING(15),
+                allowNull: false,
+                unique: true,
+              },
+            }, {
+              sequelize,
+              timestamps: true,
+              underscored: false,
+              modelName: 'Hashtag',
+              tableName: 'hashtags',
+              paranoid: false,
+              charset: 'utf8mb4',
+              collate: 'utf8mb4_general_ci',
+        });
+    }
+    static associate(db){
+        db.Hashtag.belongsToMany(db.Post, {through : 'PostHashtag'});
+    }
+}
--- models/post.js
+++ models/post.js
@@ -1,33 +1,30 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-module.exports = class Post extends Sequelize.Model {
-  static init(sequelize) {
-    return super.init(
-      {
-        content: {
-          type: Sequelize.STRING(140),
-          allowNUll: false,
-        },
-        img: {
-          type: Sequelize.STRING(200),
-          allowNUll: true,
-        },
-      },
-      {
-        sequelize,
-        timestamps: true,
-        underscored: false,
-        modelName: "Post",
-        tableName: "posts",
-        paranoid: false,
-        charset: "utf-8",
-        collate: "utf8_general_ci",
-      }
-    );
-  }
+module.exports = class Post extends Sequelize.Model{
+    static init(sequelize){
+        return super.init({
+            content : {
+                type: Sequelize.STRING(140),
+                allowNull: false,
+            },
+            img : {
+                type: Sequelize.STRING(200),
+                allowNull: true,
+            }
+        }, {
+            sequelize,
+            timestamps: true,
+            underscored: false,
+            modelName: 'Post',
+            tableName: 'posts',
+            paranoid: false,
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        })
+    }
 
-  static associate(db) {
-    db.Post.belongsTo(db.User);
-    db.Post.belongsToMany(db.Hashtag, { through: "PostHashtag" });
-  }
-};
+    static associate(db){
+        db.Post.belongsTo(db.User);
+        db.Post.belongsToMany(db.Hashtag, { through : 'PostHashtag'});
+    }
+}
