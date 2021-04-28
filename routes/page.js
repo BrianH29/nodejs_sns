ecd73630b4a9e58452ded9452c2f538d4b05571e
BrianH <brianhwang29@gmail.com>
2021-04-28 10:27:58 +0900

    modified

--- routes/page.js
+++ routes/page.js
@@ -1,29 +1,28 @@
-"use strict";
+const express = require('express');
+const {isLoggedIn, isNotLoggedIn} = require('./middlewares'); 
 
-const express = require("express");
-const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 
-const router = express.Router();
+const router = express.Router(); 
 
-router.get("/profile", isLoggedIn, (req, res) => {
-  res.render("profile", { title: "My Info - NodeBird", user: req.user });
+router.get('/profile', isLoggedIn, (req,res) => {
+    res.render('/profile)', { title : 'My Info - NodeBird'});
 });
 
-router.get("/join", isNotLoggedIn, (req, res) => {
-  res.render("join", {
-    title: "Register - Nodebird",
-    user: req.user,
-    joinError: req.flash("joinError"),
-  });
+router.get('/join', isNotLoggedIn, (req,res) => {
+    res.render('join', {
+        title : 'Register - NodeBird',
+        user: req.user,
+        joinError: req.flash('joinError')
+    });
 });
 
-router.get("/", (req, res, next) => {
-  res.render("main", {
-    title: "Nodebird",
-    twits: [],
-    user: req.user,
-    loginError: req.flash("loginError"),
-  });
+router.get('/', (req,res,next) => {
+    res.render('main', {
+        title: 'NodeBird',
+        twits: [],
+        user: req.user,
+        loginError: req.flash('loginError'),
+    })
 });
 
-module.exports = router;
+module.exports = router; 
